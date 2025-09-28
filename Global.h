15da9be427fdd71420c709b4d0f4074498c8d8d7
disi <marcus.disi@gmail.com>
2025-09-28 13:21:02 +0100

    Refactor Global.h to include additional global variables
    Added new global variables for animation, NPC keywords, faction filters, and ammunition settings.

--- Global.h
+++ Global.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <PCH.h>
 
 // Global logger pointer
@@ -8,11 +7,33 @@ extern std::shared_ptr<spdlog::logger> gLog;
 extern bool DEBUGGING;
 // Global data handler
 extern RE::TESDataHandler* g_dataHandle;
+
+// Global debug flag
+extern bool ANIMATION;
+// Global variable to store NPC keywords
+extern std::unordered_set<std::string> npcInclude;
+// Global variable to store NPC exclude EditorIDs
+extern std::unordered_set<std::string> npcExclude;
+// Global variables for faction filters
+extern std::unordered_set<std::string> includeFaction;
+extern std::unordered_set<std::string> excludeFaction;
 // Global variable for max allowed slotmask
 extern std::uint32_t maxAllowedSlotmask;
-// Global variable for excluded FormIDs
-extern std::vector<std::string> excludedFormIDs;
+// Define slot order: always check slot 33 first, then others
+extern std::vector<int> slotOrder;
+// Global variable for armor slots per cycle
+extern int armorSlotsPerCycle;
 // Global variable for excluded keywords
-extern std::vector<std::string> excludedKeywords;
+extern std::unordered_set<std::string> excludedKeywords;
 // Global variable for included keywords
-extern std::vector<std::string> includedKeywords;
+extern std::unordered_set<std::string> includedKeywords;
+// Global variable for excluded FormIDs
+extern std::unordered_set<std::string> excludedFormIDs;
+// Global variable to store Workshop objects keywords
+extern std::unordered_set<std::string> workshopObjectKeywords;
+// Global variables for ammunition
+extern int ammoMin;
+extern int ammoRefill;
+
+// Default ini file
+extern const char* defaultIni;
